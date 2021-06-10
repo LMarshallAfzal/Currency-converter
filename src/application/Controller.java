4543b0d5863dac9273fd5aa1c05d3ffb23fda28a
Leonard Marshall Afzal <k20032513>
2021-06-11 00:28:16 +0100

    I added a button that will eventually convert the currency using the current exchange rates. I added the method retrieveStartingAmount that converts the textfield input into a double. This method is called by the onSelectButtonClicked method which is the method called when the button is clicked.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,4 +1,62 @@
 package application;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    //FXML instance variables
+    @FXML
+    private TextField startingCurrencyTxt;
+
+    @FXML
+    private TextField resultingCurrencyTxt;
+
+    @FXML
+    private Button selectBtn;
+
+    @FXML
+    private ComboBox<String> startingCurrencyCmb;
+
+    @FXML
+    private ComboBox<String> resultCurrencyCmb;
+
+    //Instance variables
+    private double startingAmount;
+
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        populateComboBoxes();
+    }
+
+    public void populateComboBoxes()
+    {
+        ObservableList<String> list = FXCollections.observableArrayList("GBP", "EUR", "USD");
+        startingCurrencyCmb.setItems(list);
+        resultCurrencyCmb.setItems(list);
+
+
+    }
+
+    public void retrieveStartingAmount()
+    {
+        CharSequence cs = startingCurrencyTxt.getCharacters();
+        startingAmount = Double.parseDouble(cs.toString());
+        System.out.println(startingAmount);
+
+    }
+
+    public void onSelectButtonClicked()
+    {
+        retrieveStartingAmount();
+    }
+
 }
+
--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("CurrencyConverterGUI.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Simple calculator");
+        stage.setTitle("Currency converter");
         stage.setScene(scene);
         stage.show();
     }
